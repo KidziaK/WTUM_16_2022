c6a5594dd6bb60d43a961ed1c7c5b0f6b661e0c6
unknown <kidziak1996@gmail.com>
2022-03-24 11:53:55 +0100

    Minor updates

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     """
 
     # Load data 
-    customers = pd.read_csv("./Dane/customers.csv")
+    customers = pd.read_csv("./Data/customers.csv")
 
     # Change NaN to 0 in customers table
     customers["FN"] = np.nan_to_num(customers["FN"])
