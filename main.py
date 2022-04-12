549bd1dcd49521ff8353275ab7c7f477777c6fd3
WojciechTrybulec <wojciech.trybulec.lo@gmail.com>
2022-04-12 17:00:44 +0200

    reduced data sets

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-
+import sqlite3
 
 if __name__ == "__main__":
     """ Folder Structure
@@ -19,3 +19,14 @@ if __name__ == "__main__":
     # Change NaN to 0 in customers table
     customers["FN"] = np.nan_to_num(customers["FN"])
     customers["Active"] = np.nan_to_num(customers["Active"])
+    
+    n = 10000
+    articles = pd.read_csv("./Data/articles.csv")        
+    articles_simple = articles.iloc[1:n,]
+    
+    transactions = pd.read_csv("./Data/transactions_train.csv") 
+    
+    transactions_simple = transactions.merge(articles_simple, on="article_id").loc[:,list(transactions.columns)]
+    
+    customers_simple = customers.merge(transactions_simple, on="customer_id").loc[:,list(customers.columns)].drop_duplicates("customer_id")
+    
